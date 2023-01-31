fb21ff759a7dbe0945073bc0335ecc99f1c7a894
Ir4D <iradedova@gmail.com>
2023-01-31 19:54:15 +0200

    feat: add accordion to prices section

--- plants/index.js
+++ plants/index.js
@@ -80,6 +80,86 @@ btnService.forEach(btn => {
         optionItem[i].style.filter = '';
       };
     }
-    console.log(activeStatus);
   });
 });
+
+/* ****************************** */
+/* PRICES ACCORDION */
+/* ****************************** */
+
+const imgBlock = document.querySelectorAll('.block-img');
+const bodyBlock = document.querySelectorAll('.block-body');
+
+document.querySelector('.block-btn__basics').addEventListener('click', function() {
+  document.querySelector('.block-basics').classList.toggle('opened');
+  if (document.querySelector('.block-btn__standard').classList.contains('btn-opened')) {
+    document.querySelector('.block-standard').classList.toggle('opened');
+    document.querySelector('.block-btn__standard').classList.toggle('btn-opened');
+    for (let i = 0; i < bodyBlock.length; i++) {
+      bodyBlock[1].classList.toggle('opened');
+    }
+  } else if (document.querySelector('.block-btn__pro').classList.contains('btn-opened')) {
+    document.querySelector('.block-pro').classList.toggle('opened');
+    document.querySelector('.block-btn__pro').classList.toggle('btn-opened');
+    for (let i = 0; i < bodyBlock.length; i++) {
+      bodyBlock[2].classList.toggle('opened');
+    }
+  }
+  document.querySelector('.block-btn__basics').classList.toggle('btn-opened');
+  for (let i = 0; i < bodyBlock.length; i++) {
+    bodyBlock[0].classList.toggle('opened');
+  }
+});
+
+document.querySelector('.block-btn__standard').addEventListener('click', function() {
+  document.querySelector('.block-standard').classList.toggle('opened');
+  if (document.querySelector('.block-btn__basics').classList.contains('btn-opened')) {
+    document.querySelector('.block-basics').classList.toggle('opened');
+    document.querySelector('.block-btn__basics').classList.toggle('btn-opened');
+    for (let i = 0; i < bodyBlock.length; i++) {
+      bodyBlock[0].classList.toggle('opened');
+    }
+  } else if (document.querySelector('.block-btn__pro').classList.contains('btn-opened')) {
+    document.querySelector('.block-pro').classList.toggle('opened');
+    document.querySelector('.block-btn__pro').classList.toggle('btn-opened');
+    for (let i = 0; i < bodyBlock.length; i++) {
+      bodyBlock[2].classList.toggle('opened');
+    }
+  }
+  document.querySelector('.block-btn__standard').classList.toggle('btn-opened');
+  for (let i = 0; i < bodyBlock.length; i++) {
+    bodyBlock[1].classList.toggle('opened');
+  }
+});
+
+document.querySelector('.block-btn__pro').addEventListener('click', function() {
+  document.querySelector('.block-pro').classList.toggle('opened');
+  if (document.querySelector('.block-btn__standard').classList.contains('btn-opened')) {
+    document.querySelector('.block-standard').classList.toggle('opened');
+    document.querySelector('.block-btn__standard').classList.toggle('btn-opened');
+    for (let i = 0; i < bodyBlock.length; i++) {
+      bodyBlock[1].classList.toggle('opened');
+    }
+  } else if (document.querySelector('.block-btn__basics').classList.contains('btn-opened')) {
+    document.querySelector('.block-basics').classList.toggle('opened');
+    document.querySelector('.block-btn__basics').classList.toggle('btn-opened');
+    for (let i = 0; i < bodyBlock.length; i++) {
+      bodyBlock[0].classList.toggle('opened');
+    }
+  }
+  document.querySelector('.block-btn__pro').classList.toggle('btn-opened');
+  for (let i = 0; i < bodyBlock.length; i++) {
+    bodyBlock[2].classList.toggle('opened');
+  }
+});
+
+const btnPrices = document.querySelectorAll('.block-btn');
+
+btnPrices.forEach(item => {
+  item.addEventListener('click', function() {
+    document.querySelector('.section-contacts').scrollIntoView({
+      block: "start", 
+      behavior: "smooth"
+    });
+  });
+})
